7336fa80cbd0df9a45a09e472808b11a51769c12
Osman Kiraz <osmnkiraz11@gmail.com>
2022-05-29 22:19:05 +0300

    mongo db bağlantısı sağlandı

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost/restful_api_nodejs", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("veritabanına bağlanıldı"))
+  .catch((hata) => console.log("db bağlantısında hata! => ",hata));
+
 app.listen(3000, () => {
   console.log("3000 portundan server çalıştı");
 });
-
