4dacaf3671118459391962b742b0c21dcb524739
Osman Kiraz <osmnkiraz11@gmail.com>
2022-05-29 23:14:27 +0300

    Örnek post ve get methodları

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ require("./db/db-connection");
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.status(200).json({'mesaj':"hoşgeldin"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ mesaj: "hoşgeldin" });
+});
 
-app.post("/",(req,res)=>{
-  res.status(200).json(req.body)
-})
+app.get("/:id", (req, res) => {
+  console.log("query => ",req.query)
+  res.status(200).json({ id: req.params.id });
+});
+
+app.post("/", (req, res) => {
+  res.status(200).json(req.body);
+});
 
 app.listen(3000, () => {
   console.log("3000 portundan server çalıştı");
