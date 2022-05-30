39f3df4298000a3bef2f063b452bd58def2b72d7
Osman Kiraz <osmnkiraz11@gmail.com>
2022-05-31 00:18:49 +0300

    Updated user-router.js

--- router/user-router.js
+++ router/user-router.js
@@ -1,29 +1,65 @@
 const router = require("express").Router();
+const User = require("../models/user-model");
 
-router.get("/", (req, res) => {
-  res.json({ mesaj: "tüm kullanıcılar listenecek" });
+// This function should get all users
+router.get("/", async (req, res) => {
+  const allUsers = await User.find({});
+  res.json(allUsers);
 });
 
+// This function should get specific user
 router.get("/:id", (req, res) => {
-  res.json({ mesaj: `id değeri ${req.params.id} olan kullanıcı listeleniyor` });
+  res.json({
+    message: `id değeri ${req.params.id} olan kullanıcı listeleniyor`,
+  });
 });
 
-router.post("/",(req,res)=>{
-    res.json(req.body)
-})
+// This function should post new user
+router.post("/", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const result = await newUser.save();
+    res.json(result);
+  } catch (error) {
+    console.log("Error while adding user : ", error);
+  }
+});
 
-router.patch("/:id",(req,res)=>{
-    res.json({
-        mesaj:"patch isteği|| id: "+ req.params.id ,
-        isteginBodysi:req.body
-    })
-})
+// This function should update specific user
+router.patch("/:id", async (req, res) => {
+  try {
+    const result = await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true ,runValidators:true}
+    );
+    if (result) {
+      res.json({
+        message: ` User with ${req.params.id} id updated`,
+        value: result,
+      });
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    //  console.log("Error while updating user : ",error)
+     return res.status(404).json({ message: error });
+  }
+});
 
-router.delete("/:id" ,(req,res)=>{
-    res.json({
-        "silinen kullanıcı id'si": req.params.id
-    })
-})
-
+// This function should delete specific user
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await User.findByIdAndDelete({ _id: req.params.id });
+    if (result) {
+      return res.json({ message: "User deleted" });
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log("Error while deleting user : ", error);
+    return res.status(404).json({ message: error });
+  }
+});
 
 module.exports = router;
