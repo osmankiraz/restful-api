8001ffae4396475c8f0252ddc83d56d4923fe918
Osman Kiraz <osmnkiraz11@gmail.com>
2022-05-31 23:11:35 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./db/db-connection");
+const errorMiddleware = require("./middleware/error-middeware");
 
 // Routes
 const userRouter = require("./router/user-router");
@@ -9,11 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/users",userRouter);
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ mesaj: "hoşgeldin" });
 });
+
+app.use(errorMiddleware);
 
 app.listen(3000, () => {
   console.log("3000 portundan server çalıştı");
