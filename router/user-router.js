727016051f985328789d1f9545ddd6567579588b
Osman Kiraz <osmnkiraz11@gmail.com>
2022-06-09 22:01:38 +0300

    Updated user-router.js

--- router/user-router.js
+++ router/user-router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/user-model");
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
+const authMiddleware = require("../middleware/auth-middleware");
 
 // This function should get all users
 router.get("/", async (req, res) => {
@@ -9,12 +10,57 @@ router.get("/", async (req, res) => {
   res.json(allUsers);
 });
 
+// This function should get you
+router.get("/me", authMiddleware, (req, res) => {
+  res.json(req.user);
+});
+
+router.patch("/me", authMiddleware, async (req, res) => {
+    // delete req.body.password;
+    delete req.body.createdAt;
+    delete req.body.updatedAt;
+  
+    if (req.body.hasOwnProperty("password")) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+    }
+  
+    const { error, value } = User.joiValidationForUpdate(req.body);
+  
+    if (error) {
+      next(createError(400, error));
+    } else {
+      try {
+        const result = await User.findByIdAndUpdate(
+          { _id: req.user._id },
+          req.body,
+          { new: true, runValidators: true }
+        );
+        if (result) {
+          res.json({
+            message: ` User with ${req.user._id} id updated`,
+            value: result,
+          });
+        } else {
+          return res.status(404).json({ message: "User not found" });
+        }
+      } catch (error) {
+        //  console.log("Error while updating user : ",error)
+        // return res.status(404).json({ message: error });
+        next(createError(400, error));
+      }
+    }
+  
+
+
+});
+
 // This function should get specific user
-router.get("/:id", (req, res) => {
+/* router.get("/:id", (req, res) => {
   res.json({
     message: `id değeri ${req.params.id} olan kullanıcı listeleniyor`,
   });
 });
+ */
 
 // This function should post new user
 router.post("/", async (req, res, next) => {
@@ -38,7 +84,11 @@ router.post("/", async (req, res, next) => {
 router.post("/login", async (req, res, next) => {
   try {
     const user = await User.login(req.body.email, req.body.password);
-    res.json(user);
+    const token = await user.generateToken();
+    res.json({
+      user,
+      token,
+    });
   } catch (error) {
     next(error);
   }
